facf2e384b882e2e60eece6c4ce1e62f856a6a60
raideno56 <nadirkichou@hotmail.fr>
2026-02-01 21:03:16 +0100

    chore: cleanup dependencies

--- convex/convex.config.ts
+++ convex/convex.config.ts
@@ -1,10 +1,8 @@
-import rateLimiter from '@convex-dev/rate-limiter/convex.config.js'
-import twilio from '@convex-dev/twilio/convex.config'
-import { defineApp } from 'convex/server'
+import rateLimiter from "@convex-dev/rate-limiter/convex.config.js";
+import { defineApp } from "convex/server";
 
-const app = defineApp()
+const app = defineApp();
 
-app.use(twilio)
-app.use(rateLimiter)
+app.use(rateLimiter);
 
-export default app
+export default app;
